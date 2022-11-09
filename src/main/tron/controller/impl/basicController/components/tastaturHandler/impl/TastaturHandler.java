ff602cae01e550b6b1d86649f417e0c73def2650
Moreno <moreno.schuenemann@haw-hamburg.de>
2022-11-09 20:05:01 +0100

    TastaturHandler muss noch getestet werden

--- src/main/tron/controller/impl/basicController/components/tastaturHandler/impl/TastaturHandler.java
+++ src/main/tron/controller/impl/basicController/components/tastaturHandler/impl/TastaturHandler.java
@@ -10,12 +10,13 @@ import java.util.Date;
 import tron.controller.util.ConfigHelper;
 public class TastaturHandler implements IGetInput, IGameKey,KeyListener {
     private HashSet<String> mappedKeys;
-    private HashSet<String> pressedKeys;
-
+    private HashMap<String,Long> pressedKeys;
+    private String lastInput="";
+    private String[] moveConfigNames;
     public TastaturHandler(IGetConfig iGetConfig) {
         mappedKeys  = new HashSet<>();
-        pressedKeys=new HashSet<>();
-       String[] moveConfigNames= ConfigHelper.getMoveConfigNames();
+        pressedKeys=new HashMap<>();
+        moveConfigNames= ConfigHelper.getMoveConfigNames();
        for (int i=0;i< moveConfigNames.length;i++){
            String mappedKey=iGetConfig.getConfigVal(moveConfigNames[i]);
            mappedKeys.add(mappedKey);
@@ -24,26 +25,56 @@ public class TastaturHandler implements IGetInput, IGameKey,KeyListener {
 
     @Override
     public String getInput() {
-        return null;
+        return lastInput;
     }
 
     @Override
     public String[] getInputsForCurrentCycle() {
+        String[] arrayToReturn=new String[3];
+        for(int i=0;i< moveConfigNames.length;i++){
+            if(i%2!=0){
+                continue;
+            }
 
-        return new String[0];
+            Long timeMoveRight=pressedKeys.get(moveConfigNames[i]);
+            Long timeMoveLeft=pressedKeys.get(moveConfigNames[i+1]);
+
+            if(timeMoveLeft==null){
+                if(timeMoveRight==null){//keine haben wert
+                    arrayToReturn[i/2]="";
+                }else{//nur rechts hat wert
+                    arrayToReturn[i/2]=moveConfigNames[i];
+                }
+            }else{
+                if(timeMoveRight==null){ //nur links hat wert
+                    arrayToReturn[i/2]=moveConfigNames[i+1];
+                }else{ //beide haben wert, nehm den neuesten
+                    if(timeMoveRight>timeMoveLeft){
+                        arrayToReturn[i/2]=moveConfigNames[i];
+                    }else {
+                        arrayToReturn[i/2]=moveConfigNames[i+1];
+                    }
+                }
+            }
+
+        }
+        pressedKeys.clear();
+        return arrayToReturn;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
         String strVal= String.valueOf(e.getKeyChar());
         if(mappedKeys.contains(strVal)){
-
+            pressedKeys.put(strVal,new Date().getTime());
         }
+        lastInput=strVal;
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        keyTyped(e);
     }
 
     @Override
