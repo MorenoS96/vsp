8ab519740a1a9de12b4b03eee1db702c38de2606
Julius Elshoff <elshoff@silpion.de>
2022-11-30 16:21:42 +0100

    [ADD] displayBoard und translation Methoden

--- src/main/tron/view/impl/ViewModel.java
+++ src/main/tron/view/impl/ViewModel.java
@@ -1,11 +1,14 @@
 package tron.view.impl;
 
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import tron.model.base.persistenz.Player;
+import tron.view.basicView.components.viewHandler.impl.Coordinate;
 import tron.view.basicView.components.viewHandler.impl.ViewHandler;
 import tron.view.interfaces.IViewModel;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewModel implements IViewModel {
@@ -31,6 +34,38 @@ public class ViewModel implements IViewModel {
 
     @Override
     public void displayBoard(List<Player> players) {
-
+        for (Player player : players) {
+            view.draw(getPlayerCoordinates(player), getPlayerColor(player.getColor()));
+            view.highlightCell(new Coordinate(player.getCurrentCell().getX(), player.getCurrentCell().getY()));
+        }
     }
+
+    public List<Coordinate> getPlayerCoordinates(Player player) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        for (int i = 0; i < player.getPaintedCells().size(); i++) {
+            coordinates.add(new Coordinate(player.getPaintedCells().get(i).getX(), player.getPaintedCells().get(i).getY()));
+        }
+        coordinates.add(new Coordinate(player.getCurrentCell().getX(), player.getCurrentCell().getY()));
+        return coordinates;
+    }
+    public Color getPlayerColor(String id) {
+        switch (id) {
+            case "1":
+                return Color.BLUE;
+            case "2":
+                return Color.RED;
+            case "3":
+                return Color.GREEN;
+            case "4":
+                return Color.YELLOW;
+            case "5":
+                return Color.PURPLE;
+            case "6":
+                return Color.ORANGE;
+            default:
+                System.out.println("Keine passende Farbe gefunden bzw zu viele Spieler");
+                return null;
+        }
+    }
+
 }
