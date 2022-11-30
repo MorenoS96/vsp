2a8b487e4587e46c0c6f0c64c469d29b92f0a473
Julius Elshoff <elshoff@silpion.de>
2022-11-30 16:41:36 +0100

    [ADD] pushClick für Start Button

--- src/main/tron/view/basicView/overlays/StartMenu.java
+++ src/main/tron/view/basicView/overlays/StartMenu.java
@@ -5,12 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import tron.controller.interfaces.IControllerView;
 import tron.view.basicView.components.boardHandler.interfaces.IViewHandler;
 import tron.view.basicView.components.viewHandler.impl.ViewUtility;
 
 public class StartMenu extends VBox {
     private final Label labelReady;
     private final Button btnStart;
+    public IControllerView iControllerView;
 
     public StartMenu(String stylesheet, IViewHandler view) {
         super(20.0);
@@ -23,6 +25,7 @@ public class StartMenu extends VBox {
         btnStart = new Button("Start Game");
         btnStart.setOnAction(event -> {
             System.out.println("click!");
+            this.iControllerView.pushClick("startGame");
             view.hideOverlays();
         });
 
