cc5ed348fc47196b8042725f4fde9003a1b167dc
Julius Elshoff <elshoff@silpion.de>
2022-12-07 16:38:16 +0100

    [REF] addition of progressbar

--- src/main/tron/view/basicView/overlays/WaitingScreen.java
+++ src/main/tron/view/basicView/overlays/WaitingScreen.java
@@ -12,6 +12,7 @@ public class WaitingScreen extends VBox {
 
     private final Label labelWaitingProgress;
     private final ProgressBar progressBar;
+    private final Double progress;
     private final Label labelPlayersReady;
     public WaitingScreen(String stylesheet, IViewHandler view) {
         super(20.0);
@@ -19,12 +20,19 @@ public class WaitingScreen extends VBox {
         this.setAlignment(Pos.CENTER);
 
         labelWaitingProgress = new Label("waiting on players");
-        //TODO progressbar
-        progressBar = new ProgressBar();
+        progress = 0.1;
+        progressBar = new ProgressBar(progress);
         //TODO dynamic x and y
         labelPlayersReady = new Label("x of y players are ready");
 
+        //TODO progression of progressbar
+        /*
+        if (new player joins)
+         progress += 1 - x/y players
+         */
+
         this.getChildren().add(labelWaitingProgress);
+        this.getChildren().add(progressBar);
         this.getChildren().add(labelPlayersReady);
     }
 }
