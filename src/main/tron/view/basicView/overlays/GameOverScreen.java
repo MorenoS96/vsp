2fd98d856b9213aa0977d355357555285a257ba1
Julius Elshoff <elshoff@silpion.de>
2022-12-07 19:51:18 +0100

    [ADD] endscreen added

---
+++ src/main/tron/view/basicView/overlays/GameOverScreen.java
@@ -0,0 +1,44 @@
+package tron.view.basicView.overlays;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import tron.view.basicView.components.boardHandler.interfaces.IViewHandler;
+
+import java.util.concurrent.CountDownLatch;
+
+public class GameOverScreen extends VBox {
+    private final Label labelWinner;
+    private final Label labelCountdown;
+    private final Label labelPLayer;
+    private final Rectangle playerColor;
+    private Integer counter;
+
+    public GameOverScreen(String stylesheet, IViewHandler view) {
+        super(20.0);
+        this.getStylesheets().add(stylesheet);
+        this.setAlignment(Pos.CENTER);
+
+        labelWinner = new Label("The winner is: ");
+        HBox hb = new HBox();
+        labelPLayer = new Label("Player 1 ");
+        playerColor = new Rectangle(20, 20, 20, 20);
+        //TODO dynamisches Laden der Daten
+        playerColor.setFill(Color.RED);
+        hb.setAlignment(Pos.CENTER);
+        hb.getChildren().add(labelPLayer);
+        hb.getChildren().add(playerColor);
+        //TODO counter fixen
+        counter = 5;
+        while (counter > 0) {counter--;}
+        labelCountdown = new Label("Countdown for new Round: " + counter);
+
+        this.getChildren().add(labelWinner);
+        this.getChildren().add(hb);
+        this.getChildren().add(labelCountdown);
+    }
+}
