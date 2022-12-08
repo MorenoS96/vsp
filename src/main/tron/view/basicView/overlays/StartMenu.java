6d46201f4f108c682563dc00d71405727242b5a3
Julius Elshoff <elshoff@silpion.de>
2022-12-08 09:18:10 +0100

    [REF] dynamically loading defaultvalue

--- src/main/tron/view/basicView/overlays/StartMenu.java
+++ src/main/tron/view/basicView/overlays/StartMenu.java
@@ -7,8 +7,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import tron.controller.impl.basicController.components.configHandler.interfaces.IGetConfig;
+import tron.controller.interfaces.IConfig;
 import tron.controller.interfaces.IControllerView;
 
+import tron.lobby.interfaces.IRegistrator;
+import tron.lobby.util.InterfaceType;
 import tron.view.basicView.components.boardHandler.interfaces.IViewHandler;
 import tron.view.basicView.components.viewHandler.impl.ViewUtility;
 
@@ -18,17 +22,20 @@ public class StartMenu extends VBox {
     private final Label labelPlayerCount;
     private final Button btnConfirm;
     private final TextField inputPlayerCount;
-    public IControllerView iControllerView;
+    private IConfig iConfig;
 
     public StartMenu(String stylesheet, IViewHandler view) {
         super(20.0);
         this.getStylesheets().add(stylesheet);
         this.setAlignment(Pos.CENTER);
 
+        iConfig = (IConfig) view.getRegistrator().getInterfaceOfType(InterfaceType.IConfig);
+        int defaultPlayerCount = Integer.parseInt(iConfig.getConfigVal("defaultPlayerCount"));
+
         labelReady = new Label("Ready?");
 
         //inputfield for desired playerCount
-        labelPlayerCount = new Label("Player Count (default is 2): ");
+        labelPlayerCount = new Label("Player Count (default is " + defaultPlayerCount + " ): ");
         inputPlayerCount = new TextField ();
         HBox hb = new HBox();
         hb.getChildren().add(inputPlayerCount);
