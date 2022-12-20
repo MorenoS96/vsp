f5009d947223bfc3b8fc67af57551a3a1afb800f
Mirco <78418052+Mirwie@users.noreply.github.com>
2022-12-20 20:42:54 +0100

    senden asynchron UDP Basis impl

--- src/main/middleware/ClientStub.java
+++ src/main/middleware/ClientStub.java
@@ -2,17 +2,43 @@ package middleware;
 
 import org.json.simple.JSONObject;
 
-import java.net.Socket;
+import java.io.IOException;
+import java.net.*;
+import java.util.Objects;
 
 public class ClientStub {
-
 
     public void invoke(String funcName, String[] params, int id) {
 
     }
 
-    void send(byte[] msg, String adress, int port, String ConnectionType) { //Snychron Asynchron // TCP oder UDP
-        Socket socket;
+    //Snychron Asynchron (snyc braucht einen Rückgabe Typ also neue Methode falls wir beides machen)
+    // TCP oder UDP
+    void send(byte[] msg, String adress, int port, String ConnectionType) {
+        if (Objects.equals(ConnectionType, "UDP")) {
+
+            DatagramSocket datagramSocket = null;
+
+            try {
+                datagramSocket = new DatagramSocket();
+
+                DatagramPacket datagramPacket = new DatagramPacket(msg, msg.length, InetAddress.getByName(adress), port);
+
+                datagramSocket.send(datagramPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (datagramSocket != null) {
+                    datagramSocket.close();
+                }
+            }
+
+
+        } else if (Objects.equals(ConnectionType, "TCP")) {
+
+        } else {
+            System.out.println("Diesen Connection Type unterstützen wir nicht.");
+        }
 
     }
 
@@ -21,10 +47,12 @@ public class ClientStub {
             throw new IllegalArgumentException("Parameter Zahl und Paramater Namen sind nicht gleich viel");
         }
         JSONObject jsObject = new JSONObject();
+        jsObject.put("id", id);
+        jsObject.put("methodName", funcName);
+
         for (int i = 0; i < params.length; i++) {
             jsObject.put(paramNames, params);
         }
-        jsObject.put("methodName", funcName);
         return jsObject;
     }
 
