bf46ecdbe912e8349a8ac89b1c4bb661b38cc728
Mirco <78418052+Mirwie@users.noreply.github.com>
2022-12-21 22:58:55 +0100

    impl von TCP verschicken Asynchron, anfangen aller anderen Methoden

--- src/main/middleware/ClientStub.java
+++ src/main/middleware/ClientStub.java
@@ -2,19 +2,42 @@ package middleware;
 
 import org.json.simple.JSONObject;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.*;
 import java.util.Objects;
 
 public class ClientStub {
 
-    public void invoke(String funcName, String[] params, int id) {
+    public void invoke(String funcName, String[] params, String[] paramNames, int id) {
+        // TODO Spezifische ID?
+        // TODO ParamNames mit in die Signatur, da so das unmarshalling einfacher (mit Key) sein sollte, sonst halt Zahlen
 
+        JSONObject jsonObject = marshall(funcName, params, paramNames, id); // Doch von dem hier zu byte[] ? dann wär paramsName nicht mehr nötig.
+
+        //String adress = nameService.lookUp();
+
+        // Über jvm senden? [send]
+
+
+    }
+
+    int register(String funcName, String[] paramTypes, int id) {
+        // Eigentliche id bestimmen
+
+        String[] paramsName = new String[1];
+        JSONObject jsObject = marshall(funcName, paramTypes, paramsName, id);
+
+        //send(jsObject,readConfig.getAdress,readConfig.getPort,id(?))
+
+        // TODO ?
+        return id;
     }
 
     //Snychron Asynchron (snyc braucht einen Rückgabe Typ also neue Methode falls wir beides machen)
     // TCP oder UDP
-    void send(byte[] msg, String adress, int port, String ConnectionType) {
+    void send(byte[] msg, String adress, int port, String ConnectionType) { //Erstmal nur Asynchron
         if (Objects.equals(ConnectionType, "UDP")) {
 
             DatagramSocket datagramSocket = null;
@@ -33,8 +56,21 @@ public class ClientStub {
                 }
             }
 
-
         } else if (Objects.equals(ConnectionType, "TCP")) {
+            try {
+
+                Socket socket = new Socket(adress, port);
+
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.write(msg, 0, msg.length);
+                dataOutputStream.flush();
+
+                socket.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
 
         } else {
             System.out.println("Diesen Connection Type unterstützen wir nicht.");
@@ -56,8 +92,17 @@ public class ClientStub {
         return jsObject;
     }
 
-    public String[] lookUp() {
+
+    public String[] lookUp() { //TODO vermutlich Rückgabetyp ändern
+        //String[] registeredServices = nameService.getRegisteredServices();
+
+        //send(String[],ApplicationStub,id)
+
         return new String[0];
+    }
+
+    void error(String method, String msg) {
+        String errorMsg = "middleware.Clientstub." + method + "|" + msg;
     }
 
 }
