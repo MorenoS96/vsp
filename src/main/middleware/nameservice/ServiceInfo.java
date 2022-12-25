a4456a869ee203fb81e5e7184f850eb46b50e131
0xkon1 <github.djhgh@simplelogin.com>
2022-12-25 19:13:20 +0100

    [ADD] ServiceInfo

---
+++ src/main/middleware/nameservice/ServiceInfo.java
@@ -0,0 +1,37 @@
+package middleware.nameservice;
+
+import java.util.Arrays;
+
+class ServiceInfo {
+        private String funcName;
+        private String[] paramTypes;
+        private int id;
+        private boolean available;
+
+        public ServiceInfo(String funcName, String[] paramTypes, int id) {
+            this.funcName = funcName;
+            this.paramTypes = Arrays.copyOf(paramTypes, paramTypes.length);
+            this.id = id;
+            this.available = true;
+        }
+
+        public String getFuncName() {
+            return funcName;
+        }
+
+        public String[] getParamTypes() {
+            return Arrays.copyOf(paramTypes, paramTypes.length);
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public boolean isAvailable() {
+            return available;
+        }
+
+        public void setAvailable(boolean available) {
+            this.available = available;
+        }
+    }
