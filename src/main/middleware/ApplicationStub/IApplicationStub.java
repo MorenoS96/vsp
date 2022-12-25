ae17e36bd0d54fc4641434fbdc548ac80db77cd9
Julius Elshoff <elshoff@silpion.de>
2022-12-25 16:30:14 +0100

    [ADD] AStub for View

--- src/main/middleware/ApplicationStub/IApplicationStub.java
+++ src/main/middleware/ApplicationStub/IApplicationStub.java
@@ -4,6 +4,9 @@ import org.json.simple.JSONObject;
 
 public interface IApplicationStub {
 
+    //TODO: Implementiert alle aus den Interfaces der anderen Klassen
+    //TODO: Implementiert call und send
+    //TODO: funktioniert mit switch case
     public abstract void receiveAsynchronous(JSONObject json);
 
     JSONObject receiveSynchronous(JSONObject json);
--- src/main/middleware/NameService.java
+++ src/main/middleware/NameService.java
@@ -1,4 +1,15 @@
 package middleware;
 
 public class NameService {
+
+    //TODO was tut der NameService? Soll dieser nicht nur die Lobbys verwalten? (IP, port, id)
+    //TODO brauchen wir ApplicationStub für View?
+    //TODO id bei register ist für welche Lobby
+
+    public void register(String funcName, String[] host, int id) {
+    }
+
+    public String[] lookup(String funcName) {
+        return null;
+    }
 }
